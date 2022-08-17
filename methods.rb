af5614556edb9067c48a78bbeec16d0df3306741
onyangoouma <jacksonouma04@gmail.com>
2022-08-17 23:08:49 +0300

    commit tree caae5af0eaee595faa0790443878f88f68f0faa5

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
-# Your code here!
+# greet_ programmer
+def greet_programmer
+    puts "Hello, programmer!"
+   
+end
+
+#greet
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+#greet_with_default
+def greet_with_default(name = "programmer")
+    puts "  Hello, #{name}!"
+end
+
+#add
+def add(num1, num2)
+    num1 + num2
+  end
+
+  #halve
+  def halve(num)
+    return nil unless num.class == Integer
+    
+    num / 2
+  end
+
